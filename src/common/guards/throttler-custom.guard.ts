239b82bea77db363e8ffed7480a8a9e431e6817e
Trần Đức Hoàn <duchoanlc4@gmail.com>
2026-03-27 07:17:46 +0700

    feat: implement custom rate limiting with proxy support

--- src/common/guards/throttler-custom.guard.ts
+++ src/common/guards/throttler-custom.guard.ts
@@ -6,14 +6,19 @@ import { THROTTLER_MESSAGE_KEY } from '../decorators/throttler-message.decorator
 
 @Injectable()
 export class ThrottlerCustomGuard extends ThrottlerGuard {
+    // logic lấy đúng IP từ Proxy
+    protected async getTracker(req: Record<string, any>): Promise<string> {
+        return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction to meet your own needs
+    }
 
-  protected async throwThrottlingException(context: ExecutionContext): Promise<void> {
-    // Đọc message từ decorator @ThrottlerMessage
-    const customMessage = this.reflector.get<string>(
-      THROTTLER_MESSAGE_KEY,
-      context.getHandler(),
-    );
+    // login hiển thị lỗi bằng tiếng việt
+    protected async throwThrottlingException(context: ExecutionContext): Promise<void> {
+        // Đọc message từ decorator @ThrottlerMessage
+        const customMessage = this.reflector.get<string>(
+            THROTTLER_MESSAGE_KEY,
+            context.getHandler(),
+        );
 
-    throw new ThrottlerException(customMessage || 'Thao tác quá nhanh, vui lòng thử lại sau!');
-  }
+        throw new ThrottlerException(customMessage || 'Thao tác quá nhanh, vui lòng thử lại sau!');
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   // 1. khởi tạo ứng dụng web bình thường(HTTP)
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 'loopback');
 
   // 2. gắn thêm RabbitMQ vào ứng dụng hiện tại
   app.connectMicroservice<MicroserviceOptions>({
